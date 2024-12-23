e347ecef8b3fcd58bdae0933fdc5b789e77ff1c7
macbook <josephine.kwakye@hubtel.com>
2024-12-23 07:10:51 +0000

    added a stop watch

---
+++ stopwatch/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useRef, useState } from 'react'
+import './App.css'
+import StopWatch from './StopWatch.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+     <StopWatch />
+    </>
+  )
+}
+
+export default App
